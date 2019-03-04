e276d882eeae1748b47e7b5376d172e575314a97
AndrosTheOne <andres.ignacio.lopez.90@gmail.com>
2019-03-04 20:27:20 -0300

    Scene Cinematica Efecto "Fade In" al empezar la escena Efecto "Fade Out" al tocar "ESC" (reemplazar por imput deseado en celular) Transicion de escena (hacia Scene Menu) al tocar "Esc" o finalizar transicion de camara

---
+++ Assets/Cinematic/LevelChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+    public Animator animator;
+
+    public int levelToLoad;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Reemplazar Input por el deseado en Celular, para skipear cinematica
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            FadeToLevel(levelToLoad);
+        }
+		
+	}
+
+    public void FadeToLevel (int levelIndex)
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+     
+}
